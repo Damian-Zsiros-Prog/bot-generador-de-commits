0e98616fa98aa130d74bd51bdafeca9d31847402
Damian <damjeronzalez23@gmail.com>
2024-02-04 02:50:06 -0500

    feat: add simple-git to support reading commit history
    - This commit adds the 'simple-git' library to the project, which will be used to read the commit history and generate commit messages based on the changes.
    - This allows us to generate commit messages that are more informative and relevant to the changes made in the codebase.
    - This will make it easier for developers to understand the purpose of each commit and track the changes made over time.

--- src/index.js
+++ src/index.js
@@ -4,35 +4,58 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { config } from "dotenv";
 import { prompt } from "./prompt.js";
 import { execSync } from "child_process";
+import simpleGit from "simple-git";
 config();
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_AI_API_KEY);
+const genAI = new GoogleGenerativeAI(
+  process.env.GOOGLE_GEMINI_AI_API_KEY ||
+    "AIzaSyDRxja8eXpAxY0lnIVSx23ncNGLxwlja8A"
+);
 
 async function hasRepoGit() {
-  try {
-    execSync("git diff --staged");
-    return true;
-  } catch (error) {
-    return false;
-  }
+  const git = simpleGit();
+
+  git.checkIsRepo((error, isRepo) => {
+    if (error) {
+      console.error("Error al verificar el repositorio de Git:", error);
+      return false;
+    }
+    return isRepo;
+  });
+}
+async function getChanges() {
+  const git = simpleGit();
+  return new Promise((resolve, reject) => {
+    git.diff(["-U20"], (error, diff) => {
+      if (error) {
+        console.error("Error al obtener las diferencias:", error);
+        reject(error);
+        return;
+      }
+
+      resolve(diff);
+    });
+  });
 }
 
 async function run() {
   if (!hasRepoGit()) return console.log("No existe repositorio de git");
-  // Obtener los cambios staged desde el repositorio de Git
-  const changes = execSync("git diff --staged ").toString();
 
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  try {
+    const changes = await getChanges();
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const promptAtSend = prompt(changes);
+    const promptAtSend = prompt(changes);
 
-  const result = await model.generateContent(promptAtSend);
-  const response = await result.response;
-  const commitMessage = response.text();
+    const result = await model.generateContent(promptAtSend);
+    const response = await result.response;
+    const commitMessage = response.text();
 
-  // Mostrar el commit message por la consola
-  console.log("Commit Message:", commitMessage);
-
-  return commitMessage;
+    console.log("Commit Message:", commitMessage);
+    return commitMessage;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  return null;
 }
 
 run();
